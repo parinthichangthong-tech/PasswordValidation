3e052ad49ade68f47a2139055ba54ba4593115ec
parinthichangthong-tech <parin.thic@ku.th>
2026-07-17 18:26:54 +0700

    Add files via upload

---
+++ PasswordValidation-main/src/PasswordValidator.java
@@ -0,0 +1,12 @@
+// ดูโจทย์ spec และวิธีทำใน README.md
+public class PasswordValidator {
+
+    static final int MIN_LEN = 8;
+    static final int MAX_LEN = 20;
+
+    static boolean validate(String pw) {
+        // TODO: implement ตาม spec ใน README.md (R1-R6)
+        
+        return false;
+    }
+}
